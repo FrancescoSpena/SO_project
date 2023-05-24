e50c7ae7243a8db666769048ebe5f8a6dbbd664e
FrancescoSpena <93486076+FrancescoSpena@users.noreply.github.com>
2023-05-24 14:28:35 +0200

    Clear code, add header for scheduler

---
+++ include/scheduler.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "fake_os.h"
+#pragma once
+
+typedef struct{
+    int next_quantum;
+    int curr_quantum;
+    int pred_quantum;
+    float alpha;
+} SchedSJFArgs;
+
+//Update value of quantum
+void updateQuantum(SchedSJFArgs *args);
+//Return the process with min burst
+FakePCB *minBurst(ListHead *ready);
+//Update all event of running queue
+void updateEvent(ListHead *cpu);
+//Change a process max burst with min
+void changeProcess(FakeOS* os, FakePCB* min);
+// Routine free cpu
+void choiceProcessFreeCPU(FakeOS *os);
+// Routine busy cpu
+void choiceProcessBusyCPU(FakeOS *os, int curr_quantum);
+//Scheduler SJF preemptive
+void schedSJF(FakeOS *os, void *args_);
